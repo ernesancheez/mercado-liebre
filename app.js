af699981d391b5d160b4eb8d3b851dc5da9840b1
ErnestoC10 <sanchezernestoalejandro@gmail.com>
2021-08-18 11:03:31 -0300

    formularios done - resta search-bar

--- app.js
+++ app.js
@@ -7,3 +7,5 @@ app.listen (port, () => console.log (`Servidor funcionando en el puerto http://l
 app.use (express.static('public'));
 
 app.get ('/', (req, res) => res.sendFile (path.join (__dirname, 'views', 'home.html')));
+app.get ('/register', (req, res) => res.sendFile (path.join (__dirname, 'views', 'register.html')));
+app.get ('/login', (req, res) => res.sendFile (path.join (__dirname, 'views', 'login.html')));
